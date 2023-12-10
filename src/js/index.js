41e1f84998f90abb8b8a690f2f24261dd9cf3ea8
Gabriel Gonçalves de Oliveira <gabriellathos@gmail.com>
2023-12-09 23:06:13 -0300

    :sparkles: added contact section and js code

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+let buttonInit = document.getElementById("button-init");
+let init = document.getElementById("init");
+
+buttonInit.addEventListener("click", () => {
+  init.scrollIntoView({ behavior: "smooth" });
+});
+
+// let buttonAbout = document.getElementById("button-about");
+// let about = document.getElementById("about");
+
+// buttonAbout.addEventListener("click", () => {
+//   about.scrollIntoView({ behavior: "smooth" });
+// });
+
+let buttonTechnologies = document.getElementById("button-technologies");
+let technologies = document.getElementById("technologies");
+
+buttonTechnologies.addEventListener("click", () => {
+  technologies.scrollIntoView({ behavior: "smooth" });
+});
+
+let buttonContact = document.getElementById("button-contact");
+let contact = document.getElementById("contact");
+
+buttonContact.addEventListener("click", () => {
+  contact.scrollIntoView({ behavior: "smooth" });
+});
